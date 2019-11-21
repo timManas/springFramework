8062300cd77faea340afc4d67f20a9b80be65ae4
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-11-21 07:30:27 -0500

    Created Spring Controller prototype

---
+++ spring5WebApp/src/main/java/timanas/springframework/spring5WebApp/controllers/BookController.java
@@ -0,0 +1,39 @@
+package timanas.springframework.spring5WebApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import timanas.springframework.spring5WebApp.repositories.BookRepository;
+
+/**
+ * Created by timmanas on 2019-11-21.
+ */
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
+
+
+/**
+
+ Notes
+
+ 1. The @RequestMapping("/books") gets invoked when a request with "/book" occurs
+ - This now returns a String "books" when model is added
+
+ 2. The annotation @Controller tells Spring that this Spring MVC Controller 
+
+ */
